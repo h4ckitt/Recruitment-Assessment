c694d59f01f521fc4cc942e8fe3d7ee30bac139f
h4ckit <dammysteve10@gmail.com>
2022-07-03 18:47:26 +0100

    [change] Split the app into frontend and backend

--- repository/repo_interface.go
+++
@@ -1,6 +0,0 @@
-package repository
-
-type PhoneNumberRepository interface {
-	FetchPaginatedPhoneNumbers(offset, limit int) ([]string, error)
-	FetchPaginatedPhoneNumbersByCode(code string, offset, limit int) ([]string, error)
-}
--- service/service_interface.go
+++
@@ -1,6 +0,0 @@
-package service
-
-type NumberValidator interface {
-	Validate(string) (string, string, string, bool)
-	GetCodeFromCountry(name string) (string, error)
-}
